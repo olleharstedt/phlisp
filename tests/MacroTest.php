6233d9d4ede021ec1682fd3ff6c51ec807ba35f0
Olle <olleharstedt@yahoo.com>
2024-07-25 19:21:05 +0000

    Add phpunit

---
+++ tests/MacroTest.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+final class MacroTest extends TestCase
+{
+    public function testCanBeCreatedFromValidEmailAddress(): void
+    {
+        $this->assertTrue(true);
+    }
+}
