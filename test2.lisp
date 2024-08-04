0d71370e704860a9423bb7df9fd9e701bfdfcb3b
Olle <olleharstedt@yahoo.com>
2024-08-04 17:46:46 +0000

    CLISP test

--- test2.lisp
+++ test2.lisp
@@ -1,6 +1,27 @@
 ; CLISP test
-(defmacro test (ast)
-  (cons
-      (princ-to-string (first ast))
-      (test (rest ast))))
-(print (test (select (round (* 100 (- 1 (/ purchase_price selling_price))) 2))))
+(defun select-helper (ast)
+  (if (null ast)
+    nil
+    (cons
+      (cond
+          (format nil "~A" (first ast))
+          (select-helper (rest ast))))))
+
+(defmacro select (ast)
+  `(select-helper ',ast))
+
+(defun test (l)
+  (if (null l) NIL
+    (progn
+      (print (first l))
+      (test (rest l)))))
+
+(defun double (x)
+  (if (null x) NIL
+      (cons (* (first x) 2)
+          (double (rest x)))))
+
+; (test '(1 2 3))
+
+(print (select (round (* 100 (- 1 (/ purchase_price selling_price))) 2)))
+; (print (select (1 2 3)))
